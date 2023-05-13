5f9727e5d662e909add9d126933290decd20eee5
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-13 12:08:15 -0500

    feat: implements a basic login system with session

--- server.js
+++ server.js
@@ -1,43 +1,18 @@
 import express from 'express'
 import session from 'express-session'
 
+const DB = [ {
+    username: 'coder',
+    password: 'secret',
+    role: 'admin'
+}]
+
 const app = express()
 app.use(session({
     secret: 'victoriasecret',
     resave: true,
     saveUninitialized: true
 }))
-
-app.get('/', (req, res) => res.send('0K'))
-
-app.get('/preference', (req, res) => {
-    const address = {
-        street: 'Calle Uno',
-        zipcode: 13008,
-        city: 'Trujillo'
-    }
-    req.session.manor_address = address
-    res.send('Dirección guardada con exito')
-})
-
-app.get('/profile', (req, res) => {
-    if (!req.session.manor_address) return res.send('No registraste ninguna dirección de envio')
-    const direccion = req.session.manor_address
-    res.send(`Tu dirección de envio es: CALLE: ${direccion.street} - CIUDAD: ${direccion.city}`)
-})
-
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) return res.send('Logout error')
-    })
-    return res.send('Logout Ok')
-})
-
-const DB = [ {
-    username: 'coder',
-    password: 'secret',
-    role: 'admin'
-}]
 
 const auth = (req, res, next) => {
     if (req.session.user) return next()
@@ -54,6 +29,13 @@ app.get('/api/login', (req, res) => {
 
 app.get('/api/private', auth, (req, res) => {
     res.send('Bienvenido!!')
+})
+
+app.get('/api/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) return res.send('Logout error')
+    })
+    return res.send('Logout Ok')
 })
 
 app.listen(8080, () => console.log('Server Up'))
