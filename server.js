4f55afa4be9c2da167db401969b7287fb0568bc2
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-13 10:45:37 -0500

    feat: create signed cookies

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 
 const app = express()
-app.use(cookieParser())
+app.use(cookieParser('victoriasecret'))
 
 app.get('/', (req, res) => res.send('0K'))
 
-app.get('/cookie/set', (req, res) => {
-    res.cookie('oreo', 'Es una galleta muy rica!').send('Cookie seteada!')
+app.get('/preference', (req, res) => {
+    const address = {
+        street: 'Calle Uno',
+        zipcode: 13008,
+        city: 'Trujillo'
+    }
+    res.cookie('manor_address', JSON.stringify(address), { signed: true }).send('Dirección guardada con exito')
 })
 
-app.get('/cookie/get', (req, res) => {
-    res.send(req.cookies.oreo)
-    // res.send(req.cookies)
+app.get('/profile', (req, res) => {
+    if (!req.signedCookies.manor_address) return res.send('No registraste ninguna dirección de envio')
+    const direccion = JSON.parse(req.signedCookies.manor_address)
+    res.send(`Tu dirección de envio es: CALLE: ${direccion.street} - CIUDAD: ${direccion.city}`)
 })
 
 app.listen(8080, () => console.log('Server Up'))
